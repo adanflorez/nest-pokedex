45bab61f46bb7196e288f92c379b5518ef81a937
Adan Florez <aflorez@rapicredit.com>
2026-02-13 17:35:20 -0500

    Refactor Pokemon service and controller to improve find and update methods with better error handling

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,10 +2,11 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { MongoServerError } from 'mongodb';
@@ -26,15 +27,7 @@ export class PokemonService {
       const pokemonSaved = await this.pokemonModel.create(pokemon);
       return pokemonSaved;
     } catch (error) {
-      if (error instanceof MongoServerError && error.code === 11000) {
-        throw new BadRequestException(
-          `Pokemon with no ${createPokemonDto.no} already exists`,
-        );
-      }
-      console.log(error);
-      throw new InternalServerErrorException(
-        'Error creating pokemon - Check logs',
-      );
+      this.handleExceptions(error);
     }
   }
 
@@ -42,15 +35,63 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon | null;
+
+    // Buscar por número
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: +term });
+      if (pokemon) return pokemon;
+    }
+
+    // Buscar por MongoID
+    if (isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+      if (pokemon) return pokemon;
+    }
+
+    // Buscar por nombre
+    pokemon = await this.pokemonModel.findOne({
+      name: term.toLowerCase(),
+    });
+
+    if (!pokemon) {
+      throw new NotFoundException(
+        `Pokemon with id, name or no "${term}" not found`,
+      );
+    }
+
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    try {
+      const pokemonModel = await this.findOne(`${term}`);
+      const currentPokemon: UpdatePokemonDto = {
+        ...updatePokemonDto,
+        name: updatePokemonDto?.name?.toLocaleLowerCase(),
+      };
+      await pokemonModel.updateOne(currentPokemon);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return { ...pokemonModel.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne(id);
+    await pokemon.deleteOne();
+  }
+
+  private handleExceptions(error: unknown) {
+    if (error instanceof MongoServerError && error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon already exists in database - ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    throw new InternalServerErrorException(
+      `Check server logs - ${String(error)}`,
+    );
   }
 }
