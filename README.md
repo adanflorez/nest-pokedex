5aea3f638012922f366a9fa532627f1fbeafbd98
Adan Florez <aflorez@rapicredit.com>
2026-02-18 09:34:51 -0500

    Add environment configuration, validation schema, and MongoDB connection settings

--- README.md
+++ README.md
@@ -19,7 +19,17 @@ npm i -g @nestjs/cli
 docker compose up -d
 ```
 
-5. Reconstruir la BD con la semilla
+5. Clonar el archivo __.env.template__ y renombrar a __.env__
+
+6. Llenar las variables de entorno definidas en el __.env__
+
+7. Ejecutar la aplicacion en dev:
+
+```
+yarn start:dev
+```
+
+8. Reconstruir la BD con la semilla
 ```
 http://localhost:3000/api/seed
 ```
--- src/app.module.ts
+++ src/app.module.ts
@@ -9,20 +9,29 @@ import { CommonModule } from './common/common.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.schemas';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot(process.env.MONGODB!, {}),
+    MongooseModule.forRoot(process.env.MONGODB!, {
+      dbName: 'pokemonsdb',
+    }),
     PokemonModule,
     CommonModule,
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log('AppModule initialized');
+    console.log(`running in port ${process.env.PORT}`);
+  }
+}
